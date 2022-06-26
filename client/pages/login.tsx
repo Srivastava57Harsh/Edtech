b8dc0a5c11d9641285f4874f8aae180159fb5bdf
Mohd Zaid <zaid.tab123@gmail.com>
2022-06-26 22:41:34 +0530

    🐛 fix otp comp re render

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,73 +1,75 @@
 import type { NextPage } from 'next';
 import Router from 'next/router';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { FormEvent, useEffect } from 'react';
+import { ToastContainer } from 'react-toastify';
+import { sendToast } from '../shared/helper/toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { validation } from '../shared/helper/validator';
-import { loginSchema } from '../shared/models/loginSchema';
+import { loginSchema, phoneValidate } from '../shared/models/loginSchema';
 
-const loginPage: NextPage = () => {
-  async function loginUser(event: FormEvent<HTMLFormElement>) {
-    try {
-      event.preventDefault();
-      const formData = {
-        phone: (event.currentTarget.elements[0] as HTMLInputElement).value,
-        otp: (event.currentTarget.elements[1] as HTMLInputElement).value,
-      };
-      console.log(formData);
-      await validation(formData, loginSchema);
-
-      // if (!localStorage.getItem('jwtToken')) {
-      //   const response = await axios({
-      //     method: 'post',
-      //     url: './api/user/login',
-      //     data: formData,
-      //   });
-      //   localStorage.setItem('jwtToken', response.data.jwtToken);
-      // }
-
-      await Router.push('/dashboard');
-    } catch (err: any) {
-      console.log(err);
-      //sendToast(err.message || 'Something went wrong', 'warn');
-    }
-  }
-
-  const [showOTPField, setOTPField] = useState(false);
-  const onClick = () => setOTPField(true);
-
-  const GetOTP = () => (
-    <>
-      <button
-        className="mt-10 rounded-xl bg-primary min-w-[150px] text-xl
-                      h-12 hover:shadow-md hover:shadow-primary-500/40 text-white"
-        onClick={onClick}
-      >
-        Get OTP
-      </button>
-    </>
-  );
-
-  const SubmitOTP = () => (
+const OTPInput: React.FC<{ setOtpInput: React.Dispatch<React.SetStateAction<number | undefined>> }> = ({
+  setOtpInput,
+}) => {
+  return (
     <>
       <input
-        className="mt-4 pl-6 border border-slate-600 text-xl font-bold
-                          bg-dark-background active:border-primary focus:text-primary-orange rounded-xl h-12 min-w-[14px] active:shadow-2xl
-                           focus:outline-none focus:border-primary
-                          focus:ring-1 focus:ring-primary focus:bg-elevated"
+        className="mt-4 pl-6 border border-slate-600 text-xl font-bold bg-dark-background active:border-primary focus:text-primary-orange rounded-xl h-12 min-w-[14px] active:shadow-2xl transition duration-300 ease-in-out
+                         focus:outline-none focus:border-primary
+                        focus:ring-1 focus:ring-primary focus:bg-elevated"
         type="number"
         name="OTP"
         id="otp"
+        maxLength={6}
         placeholder="OTP"
+        onChange={e => {
+          setOtpInput(e.target.valueAsNumber);
+        }}
       />
-      <button
-        className="mt-10 rounded-xl bg-primary min-w-[150px] text-xl
-                      h-12 hover:shadow-md hover:shadow-primary-500/40 text-white"
-        onClick={onClick}
-      >
-        Login
-      </button>
     </>
   );
+};
+
+const loginPage: NextPage = () => {
+  const [showOTPField, setOTPField] = useState(false);
+  const [phoneValid, setPhoneValid] = useState(false);
+  const [phoneInput, setPhoneInput] = useState('');
+  const [otpInput, setOtpInput] = useState<number>();
+  // const [validationError, setValidateError] = useState();
+
+  async function loginUser() {
+    try {
+      const formData = {
+        phone: phoneInput,
+        otp: otpInput,
+      };
+      console.log(formData);
+      await validation(formData, loginSchema);
+      await Router.push('/dashboard');
+    } catch (err: any) {
+      console.log(err);
+      sendToast(err.message || 'Something went wrong', 'warn');
+    }
+  }
+
+  const sendOTP = async () => {
+    setOTPField(true);
+    await sendToast('OTP sent', 'success');
+  };
+
+  const validatePhone = async (event: any) => {
+    try {
+      event.preventDefault();
+      const formData = {
+        phone: phoneInput,
+      };
+      await validation(formData, phoneValidate);
+      setPhoneValid(true);
+      await sendOTP();
+    } catch (err) {
+      console.log({ err });
+    }
+  };
 
   useEffect(() => {
     if (localStorage.getItem('jwtToken')) {
@@ -77,14 +79,9 @@ const loginPage: NextPage = () => {
 
   return (
     <>
-      {/*
-      <ToastContainer /> */}
+      <ToastContainer />
 
-      <form
-        className="flex flex-col justify-center items-center text-primary min-w-full min-h-screen bg-gradient-to-r from-background to-bg-blue"
-        onSubmit={loginUser}
-        method="POST"
-      >
+      <div className="flex flex-col justify-center items-center text-primary min-w-full min-h-screen bg-gradient-to-r from-background to-bg-blue">
         <h2 className="font-black text-5xl pb-2">
           Databuddy<span className="text-primary-orange">.</span>
         </h2>
@@ -93,16 +90,27 @@ const loginPage: NextPage = () => {
           <input
             className="pl-6 border border-slate-600 text-xl font-bold
                           bg-dark-background active:border-primary focus:text-primary-orange rounded-xl h-12 min-w-[14px] active:drop-shadow-xl focus:shadow-black
-                           focus:outline-none focus:border-primary
+                           focus:outline-none focus:border-primary transition duration-300 ease-in-out
                           focus:ring-1 focus:ring-primary focus:bg-elevated"
             type="number"
-            name="name"
+            name="phone"
+            max={10}
             placeholder="Phone"
-            id="name"
+            onChange={e => {
+              setPhoneInput(e.target.value);
+            }}
+            id="phone"
           />
-          {showOTPField ? <SubmitOTP /> : <GetOTP />}
+          {showOTPField && phoneValid ? <OTPInput setOtpInput={setOtpInput} /> : null}
+          <button
+            className="mt-10 rounded-xl bg-primary min-w-[150px] text-xl hover:-translate-y-1 transition duration-500 ease-in-out hover:bg-primary-orange
+                      h-12 hover:shadow-md hover:shadow-primary-500/40 text-white"
+            onClick={phoneValid ? loginUser : validatePhone}
+          >
+            {showOTPField && phoneValid ? 'Login' : 'Get OTP'}
+          </button>
         </div>
-      </form>
+      </div>
     </>
   );
 };
