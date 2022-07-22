8a633efcab585cc0680b6327683b60e0eea821a0
Mohd Zaid <zaid.tab123@gmail.com>
2022-07-22 19:08:49 +0530

    ✨ create dashboard structure

---
+++ client/components/dashboard/sidenavigation/data.js
@@ -0,0 +1,17 @@
+import HomeIcon from './icons/home';
+import MyCoursesIcon from './icons/mycourses';
+
+const data = [
+  {
+    title: 'Home',
+    icon: <HomeIcon />,
+    link: '/',
+  },
+  {
+    title: 'My Courses',
+    icon: <MyCoursesIcon />,
+    link: '/admin/my-courses',
+  },
+];
+
+export default data;
--- client/pages/dashboard.tsx
+++ client/pages/dashboard.tsx
@@ -1,9 +1,29 @@
 import { NextPage } from 'next';
+import SideNavigation from '../components/dashboard/sidenavigation';
+import TopNavigation from '../components/dashboard/topnavigation';
+import DashboardProvider from '../components/dashboard/provider/context';
+import Overlay from '../components/dashboard/provider/overlay';
+
+const style = {
+  container: `bg-gray-100 h-screen overflow-hidden relative`,
+  main: `h-screen overflow-auto pb-36 pt-4 px-2 md:pb-8 lg:px-4`,
+  mainContainer: `flex flex-col h-screen pl-0 w-full lg:pl-24 lg:space-y-4`,
+};
 
 const dashboard: NextPage = () => {
   return (
     <>
-      <h1>Dashboard</h1>
+      <DashboardProvider>
+        <div className={style.container}>
+          <div className="flex items-start">
+            <Overlay />
+            <SideNavigation mobilePosition="right" />
+            <div className={style.mainContainer}>
+              <TopNavigation />
+            </div>
+          </div>
+        </div>
+      </DashboardProvider>
     </>
   );
 };
