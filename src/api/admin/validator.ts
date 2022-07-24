d7a7fe398a47e389d71a4ad68dd8e077134b3370
Srivastava57Harsh <srivastava57harsh@gmail.com>
2022-07-25 03:21:34 +0530

    :sparkles: Add course and admin
    Add course and admin

---
+++ src/api/admin/validator.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response } from 'express';
+import LoggerInstance from '../../loaders/logger';
+import { addCourseSchema, courseDataSchema, getProfileSchema, loginSchema } from './schema';
+
+export async function loginValidator(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    req.body = await loginSchema.validate(req.body, { stripUnknown: true });
+    next();
+  } catch (e) {
+    LoggerInstance.error(e);
+    res.status(422).json({
+      message: 'Validation Failed',
+      error: e.errors.map(error => error),
+    });
+  }
+}
+
+export async function getProfileValidator(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    req.body = await getProfileSchema.validate(req.headers);
+    next();
+  } catch (e) {
+    LoggerInstance.error(e);
+    res.status(422).json({
+      message: 'Token Required',
+      error: e.errors.map(error => error),
+    });
+  }
+}
+
+export async function addCourseValidator(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    req.body = await addCourseSchema.validate(req.body, { stripUnknown: true });
+    for (let i = 0; i < req.body.data.length; i++) {
+      await courseDataSchema.validate(req.body.data[i], { stripUnknown: true });
+    }
+
+    next();
+  } catch (e) {
+    LoggerInstance.error(e);
+    res.status(422).json({
+      message: 'Validation field',
+      error: e.errors.map(error => error),
+    });
+  }
+}
