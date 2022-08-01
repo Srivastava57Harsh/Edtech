7bee07f9c8b1b3f259c3947b930b137b78961223
Mohd Zaid <zaid.tab123@gmail.com>
2022-08-01 18:22:59 +0530

    ✨ reset password page

--- client/pages/reset/[uid]/index.tsx
+++ client/pages/reset/[uid]/index.tsx
@@ -1,38 +1,35 @@
 import axios from 'axios';
-import { useRouter } from 'next/router';
+import Router from 'next/router';
 import { FormEvent, useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { API_URL } from '../../../config';
 import { sendToast } from '../../../shared/helper/toastify';
-import { handleResetPassword } from '../../../shared/helper/axios';
+import { handleResetAuth } from '../../../shared/helper/axios';
 
-const ResetPassword = () => {
-  const router = useRouter();
-  const { uid, token } = router.query;
-
+const signUpPage = () => {
   const [userData, setUserData] = useState<any>({});
 
-  async function ResetPassword(event: FormEvent<HTMLFormElement>) {
+  async function SignUpUser(event: FormEvent<HTMLFormElement>) {
     try {
       event.preventDefault();
       const formData = {
-        password: (event.currentTarget.elements[0] as HTMLInputElement).value,
-        passwordConfirmation: (event.currentTarget.elements[1] as HTMLInputElement).value,
+        email: (event.currentTarget.elements[0] as HTMLInputElement).value,
+        secretQuestion: (event.currentTarget.elements[1] as HTMLInputElement).value,
+        secretAnswer: (event.currentTarget.elements[2] as HTMLInputElement).value,
       };
       console.log(formData);
       //   await validation(formData, signupSchema);
       const body = {
-        newPassword: formData.password,
-        id: { uid },
+        email: formData.email,
+        secretQuestion: formData.secretQuestion,
+        secretAnswer: formData.secretAnswer,
       };
       console.log(body);
       setUserData(body);
 
-      const response = await handleResetPassword(body);
-
+      const response = await handleResetAuth(body);
       if (response) {
-        await sendToast('Password has been reset.', 'success');
-        console.log('done');
+        await Router.push(`/reset/${response.data.userId}/${response.data.resetToken}`);
       }
     } catch (err: any) {
       console.log(err);
@@ -40,10 +37,10 @@ const ResetPassword = () => {
     }
   }
 
-  const ResetForm = () => (
+  const ResetAuthForm = () => (
     <form
       className="flex flex-col justify-center items-center text-primary min-w-full min-h-screen bg-gradient-to-r from-background to-bg-blue"
-      onSubmit={ResetPassword}
+      onSubmit={SignUpUser}
       method="POST"
     >
       <h2 className="font-black text-5xl pb-2">
@@ -52,46 +49,64 @@ const ResetPassword = () => {
       <h3 className="mb-4 text-slate-600">Learn with technology</h3>
       <h2 className="mb-4 text-2xl text-orange-500">Recover your Password</h2>
       <div className="flex flex-col items-center  w-2/3 sm:w-1/3 ">
-        <label
-          className="mt-4 pl-1 text-md w-[360px] font-thick text-gray-500
-        "
-        >
-          Enter new password
-        </label>
+        <label className="mt-4 pl-1 text-md w-[360px] font-thick text-gray-500">Enter your registered email-id</label>
         <input
           className="mt-4 pl-4 border border-slate-600 text-md w-[360px]
                           bg-dark-background active:border-primary focus:text-primary-orange rounded-xl h-12 active:drop-shadow-xl focus:shadow-black
                           focus:outline-none focus:border-primary
                           focus:ring-1 focus:ring-primary focus:bg-elevated"
-          type="password"
-          name="password"
-          placeholder="Password"
-          id="password"
+          type="email"
+          name="email"
+          placeholder="E-Mail"
+          id="email"
         />
         <label
           className="mt-4 pl-1 text-md w-[360px] font-thick text-gray-500
         "
         >
-          Confirm new password
+          Enter the secret question you had entered <br /> while creating your account
+        </label>
+        <select
+          className="mt-4 pl-4 border border-slate-600 text-md w-[360px]
+                          bg-dark-background active:border-primary focus:text-primary-orange rounded-xl h-12 active:drop-shadow-xl focus:shadow-black
+                          focus:outline-none focus:border-primary
+                          focus:ring-1 focus:ring-primary focus:bg-elevated"
+          name="Secret Question"
+          placeholder="Secret Question"
+          id="confirm-password"
+        >
+          <option value="" disabled selected>
+            Secret Question
+          </option>
+          <option value="What is your pet name?">What is your pet name?</option>
+          <option value="What is your birth city?">What is your birth city?</option>
+          <option value="What is your car name?">What is your car name?</option>
+          <option value="What is your school name?">What is your school name?</option>
+        </select>
+        <label
+          className="mt-4 pl-1 text-md w-[360px] font-thick text-gray-500
+        "
+        >
+          Enter the secret answer you had entered <br /> while creating your account
         </label>
         <input
           className="mt-4 pl-4 border border-slate-600 text-md w-[360px]
                           bg-dark-background active:border-primary focus:text-primary-orange rounded-xl h-12 active:drop-shadow-xl focus:shadow-black
                           focus:outline-none focus:border-primary
                           focus:ring-1 focus:ring-primary focus:bg-elevated"
-          type="password"
-          name="confirm-password"
-          placeholder="Confirm Password"
+          type="answer"
+          name="Secret Answer"
+          placeholder="Secret Answer"
           id="confirm-password"
         />
       </div>
       <button
-        className="mt-10 rounded-xl bg-primary min-w-[200px] text-xl hover:-translate-y-1 transition duration-500 ease-in-out hover:bg-primary-orange
+        className="mt-10 rounded-xl bg-primary min-w-[150px] text-xl hover:-translate-y-1 transition duration-500 ease-in-out hover:bg-primary-orange
         h-12 hover:shadow-md hover:shadow-primary-500/40 text-white"
         type="submit"
-        id="reset-button"
+        id="sign-up-button"
       >
-        Reset Password
+        Proceed
       </button>
     </form>
   );
@@ -106,10 +121,9 @@ const ResetPassword = () => {
   return (
     <>
       <ToastContainer />
-      <ResetForm />
-      <p>Post: {uid}</p>
+      <ResetAuthForm />
     </>
   );
 };
 
-export default ResetPassword;
+export default signUpPage;
