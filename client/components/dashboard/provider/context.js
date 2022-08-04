fba35806424b2c767abc4624af3f6bb00d7be30e
Mohd Zaid <zaid.tab123@gmail.com>
2022-08-04 23:54:35 +0530

    🐛 fix scroll

--- client/components/dashboard/provider/context.js
+++ client/components/dashboard/provider/context.js
@@ -10,13 +10,13 @@ export default function DashboardProvider({ children }) {
   const router = useRouter();
 
   const toggle = React.useCallback(() => {
-    setOpen((prevState) => !prevState);
+    setOpen(prevState => !prevState);
   }, []);
 
   // set the html tag style overflow to hidden
-  React.useEffect(() => {
-    document.documentElement.style.overflow = 'hidden';
-  });
+  // React.useEffect(() => {
+  //   document.documentElement.style.overflow = 'hidden';
+  // });
 
   // close side navigation when route changes
   React.useEffect(() => {
@@ -33,7 +33,7 @@ export default function DashboardProvider({ children }) {
 
   // close side navigation on click outside
   React.useEffect(() => {
-    const handleOutsideClick = (event) => {
+    const handleOutsideClick = event => {
       if (!ref.current?.contains(event.target)) {
         if (!open) return;
         setOpen(false);
@@ -43,11 +43,7 @@ export default function DashboardProvider({ children }) {
     return () => window.removeEventListener('click', handleOutsideClick);
   }, [open, ref]);
 
-  return (
-    <Context.Provider value={{ open, ref, toggle }}>
-      {children}
-    </Context.Provider>
-  );
+  return <Context.Provider value={{ open, ref, toggle }}>{children}</Context.Provider>;
 }
 
 // custom hook to consume all context values { open, ref, toggle }
